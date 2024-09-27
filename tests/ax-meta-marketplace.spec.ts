a178fa284df37e6fb4c9f96ae894aca487e04920
g-ouattara <idrissgouattara@gmail.com>
2024-09-27 11:47:12 +0000

    updated

--- tests/ax-meta-marketplace.spec.ts
+++ tests/ax-meta-marketplace.spec.ts
@@ -9,13 +9,13 @@ test("Le premier produit sur le marketplace et Ax-Meta Pavillon 15", async ({
 	await expect(page.locator("#root")).toContainText("Ax-Meta Pavillon 15");
 });
 
-test("Le premier produit  dans l’onglet des tablettes est Ax-Meta Tab Pro 912.9 pouces", async ({
+test("Le premier produit  dans l’onglet des tablettes est Ax-Meta Tab Pro 12.9 pouces", async ({
 	page,
 }) => {
 	await page.goto("https://ax-meta.com/");
 	await page.getByRole("link", { name: "MARKETPLACE" }).click();
 	await page.getByRole("link", { name: "Tablettes" }).click();
 	await expect(page.locator("#root")).toContainText(
-		"Ax-Meta Tab Pro 12.9 pouces",
+		"Ax-Meta Tab Pro 912.9 pouces",
 	);
 });
