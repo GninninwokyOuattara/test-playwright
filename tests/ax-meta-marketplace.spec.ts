bce5e47f96aab23c501fa7f3e7ba96d6f2251530
g-ouattara <idrissgouattara@gmail.com>
2024-09-27 16:14:14 +0000

    updated

--- tests/ax-meta-marketplace.spec.ts
+++ tests/ax-meta-marketplace.spec.ts
@@ -16,6 +16,6 @@ test("Le premier produit  dans l’onglet des tablettes est Ax-Meta Tab Pro 12.9
 	await page.getByRole("link", { name: "MARKETPLACE" }).click();
 	await page.getByRole("link", { name: "Tablettes" }).click();
 	await expect(page.locator("#root")).toContainText(
-		"Ax-Meta Tab Pro 912.9 pouces",
+		"Ax-Meta Tab Pro 12.9 pouces",
 	);
 });
